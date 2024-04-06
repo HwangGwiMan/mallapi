848dda8b3cb9fb2f6d3231001f14fce719c476fa
HwangGwiMan <eze886@daum.net>
2024-04-06 14:00:48 +0900

    frontend start

---
+++ frontend/src/pages/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import BasicLayout from "../layouts/BasicLayout";
+
+
+function AboutPage(props) {
+    return (
+        <BasicLayout>
+            <div className="text-3xl">AboutPage</div>
+        </BasicLayout>
+    )
+}
+
+export default AboutPage;
