2319398e413fb1cbc23ac40946e33d135e8a9708
HwangGwiMan <eze886@daum.net>
2024-04-08 17:32:56 +0900

    Todo List View

--- src/main/java/org/zerock/mallapi/controller/TodoController.java
+++ src/main/java/org/zerock/mallapi/controller/TodoController.java
@@ -3,8 +3,6 @@ package org.zerock.mallapi.controller;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.jaxb.SpringDataJaxb.PageRequestDto;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.mallapi.dto.PageRequestDTO;
@@ -18,7 +16,6 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
